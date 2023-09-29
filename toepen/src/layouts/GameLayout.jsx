f88c9f662c0d97620f5244e973cb09423c81c363
JulianW051 <julianwessels1@gmail.com>
2023-09-29 11:10:03 +0200

    start realisatie

--- toepen/src/layouts/GameLayout.jsx
+++ toepen/src/layouts/GameLayout.jsx
@@ -9,6 +9,42 @@ export default function GameLayout() {
     
     return (
         <div className="game">
+          <div className="top-bar">
+            <div className="top-left">
+
+            </div>
+            <div className="top-mid">
+              <img src="https://media.discordapp.net/attachments/894538357793767474/1156930224735387708/card_back_red.png?ex=6516c2bc&is=6515713c&hm=431ce0916d13f0252783f95e0b487e77c0e6cb34e84bf289f3d716668724cc06&=&width=465&height=662" alt="kaart" />
+              <img src="https://media.discordapp.net/attachments/894538357793767474/1156930224735387708/card_back_red.png?ex=6516c2bc&is=6515713c&hm=431ce0916d13f0252783f95e0b487e77c0e6cb34e84bf289f3d716668724cc06&=&width=465&height=662" alt="kaart" />
+              <img src="https://media.discordapp.net/attachments/894538357793767474/1156930224735387708/card_back_red.png?ex=6516c2bc&is=6515713c&hm=431ce0916d13f0252783f95e0b487e77c0e6cb34e84bf289f3d716668724cc06&=&width=465&height=662" alt="kaart" />
+              <img src="https://media.discordapp.net/attachments/894538357793767474/1156930224735387708/card_back_red.png?ex=6516c2bc&is=6515713c&hm=431ce0916d13f0252783f95e0b487e77c0e6cb34e84bf289f3d716668724cc06&=&width=465&height=662" alt="kaart" />
+            </div>
+            <div className="top-right">
+              
+            </div>
+          </div>
+          <div className="mid-bar">
+            <div className="mid-left">
+
+            </div>
+            <div className="mid-mid">
+
+            </div>
+            <div className="mid-right">
+              
+            </div>
+          </div>
+          <div className="bottom-bar">
+            <div className="bottom-left">
+
+            </div>
+            <div className="bottom-mid">
+
+            </div>
+            <div className="bottom-right">
+              
+            </div>
+          </div>
         </div>
     )
 }
